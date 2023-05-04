78f1af66036cd696a7ecff750d258952b54c6112
idowufaith-data <idowufaith4u@gmail.com>
2023-05-04 15:03:59 +0100

    commit

---
+++ immutable.py
@@ -0,0 +1,9 @@
+#Strings are immutable, unordered, contains numbers or character in single, double or triple quotes
+my_string = "Hello World" #strings are created with single, double or triple quotes
+print(my_string)
+
+
+#Tuple are unordered, contains different data types, immutable, and does not allow duplicates
+my_tuple = tuple((1, "name", True))
+my_tuple = (1, "name", True)
+print(my_tuple)
---
+++ mutable.py
@@ -0,0 +1,25 @@
+#Lists are mutable, ordered, contains different data types and allows duplicates
+my_list = list([1, "name", True]) #this command also creates a list
+my_list = [ 1, "name", True] #the output will be given in this ordered; i.e lists are ordered
+
+my_list[1] = "faith" #lists are mutable; name has been permanently changed to faith in the list
+
+my_list.append("idowu") #lists are mutable; idowu has been added to the list
+
+my_list.remove(1) #will remove the number 1 from the list
+
+my_list.insert(2, False) #inserts False at index number 2
+
+my_list.reverse() #reverses the items in the lists and prints them from the last to the first
+
+my_list.sort() #sorts the items in ascending order it alters the list
+
+my_list.pop() #returns the last item from a list and also removes that item from the list
+
+my_list.clear() #clears all the items in the list
+
+#Sets are mutable, ordered, allow different data types do not allow duplicates
+my_set = {1, 2, 3, 4, 5}
+my_set = set({1, 2, 3, 4, 5})
+
+#Dictionary are mutable, ordered, are created in the as keys:values, allows duplicates
